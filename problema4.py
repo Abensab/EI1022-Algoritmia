ad64a7d1ff5abd3c4fe1892febe43f6ad5f73c9d
Abraham Benitez Sabao <al292072@uji.es>
2018-10-03 17:49:06 +0200

    Printing matrix and correct jumps for horse in matrix.

--- problema4.py
+++ problema4.py
@@ -6,72 +6,74 @@ from Utils.graph2dviewer import Graph2dViewer
 
 def horse_graph(rows, cols):
     edges = []
-    saltos = [(1, -2), (2, -1), (2, 1), (1, 2)]
+    jumps = [(1, -2), (2, -1), (2, 1), (1, 2)]
     for r in range(rows):
         for c in range(cols):
-            for (ri, ci) in saltos:
+            for (ri, ci) in jumps:
                 if r + ri < rows and 0 <= c + ci < cols:
                     edges.append(((r, c), (r + ri, c + ci)))
 
     return UndirectedGraph(E=edges)
 
 
-def matriz_saltos(grafo, num_rows, num_cols):
+def jump_matrix(grafo, num_rows, num_cols):
     m = []
     v_inicial = (0, 0)
     for r in range(num_rows):
         m.append([0] * num_cols)
 
-    # TODO: añadir matriz de saltos
     vertices = []
     queue = Fifo()
     seen = set()
     queue.push(v_inicial)
     seen.add(v_inicial)
-    salto = 1
     while len(queue) > 0:
         v = queue.pop()
         vertices.append(v)
         for suc in grafo.succs(v):
             if suc not in seen:
                 seen.add(suc)
-                m[suc[0]][suc[1]] = salto
+                m[suc[0]][suc[1]] = m[v[0]][v[1]]+1
                 queue.push(suc)
-        salto += 1
-    # ---------------
 
     return m
 
 
-def recorredor_vertices_anchura(grafo, v_inicial):
-    vertices = []
+def travel_vertex_width(grafo, v_inicial):
+    edges = []
     queue = Fifo()
     seen = set()
     queue.push(v_inicial)
     seen.add(v_inicial)
     while len(queue) > 0:
         v = queue.pop()
-        vertices.append(v)
+        edges.append(v)
         for suc in grafo.succs(v):
             if suc not in seen:
                 seen.add(suc)
                 queue.push(suc)
-    return vertices
+    return edges
 
 
-def num_casillas_alcanzables(grafo, v_inicial):
-    return len(recorredor_vertices_anchura(grafo_tablero, v_inicial))
+def num_achievable_spaces(graph, v_initial):
+    return len(travel_vertex_width(graph, v_initial))
 
+def format_matrix(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('[{{:{}}}]'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    return '\n'.join(table)
 
 if __name__ == '__main__':
     num_rows = 8
     num_cols = 8
-    grafo_tablero = horse_graph(num_rows, num_cols)
+    table_graph = horse_graph(num_rows, num_cols)
 
     source = (0, 3)
-    print("Número de casillas alcanzable desde", source, ": ", num_casillas_alcanzables(grafo_tablero, source))
-    matriz = matriz_saltos(grafo_tablero, num_rows, num_cols)
+    print("Número de casillas alcanzable desde", source, ": ", num_achievable_spaces(table_graph, source))
+    matrix = jump_matrix(table_graph, num_rows, num_cols)
 
-    [print(i) for i in matriz]
-    viewer = Graph2dViewer(grafo_tablero, window_size=(400, 400), vertexmode=Graph2dViewer.ROW_COL)
+    print(format_matrix(matrix))
+    viewer = Graph2dViewer(table_graph, window_size=(400, 400), vertexmode=Graph2dViewer.ROW_COL)
     viewer.run()
