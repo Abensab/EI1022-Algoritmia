96743d095f713d4b09a430472a7fb4e8c6cc3c48
Abraham Benitez Sabao <al292072@uji.es>
2019-01-16 13:21:39 +0100

    Ejercicio voraces Enero 2015

---
+++ Examenes/Enero2015_Ejercicio_4.py
@@ -0,0 +1,27 @@
+from typing import *
+
+
+def coloca_seguratas(pos_cuadros: List[float], D : float, L : float) -> List[float]:
+    res = []
+    indices_ordenados = sorted(range(len(pos_cuadros)), key=lambda i: pos_cuadros[i])
+
+    for x in indices_ordenados:
+        if len(res) == 0:
+            if pos_cuadros[x] + D < L:
+                res.append(pos_cuadros[x] + D)
+            else:
+                res.append(pos_cuadros[x])
+        else:
+            if res[-1] + D < pos_cuadros[x] and pos_cuadros[x] + D < L:
+                res.append(pos_cuadros[x]+D)
+            elif res[-1] + D < pos_cuadros[x] and pos_cuadros[x] + D >= L:
+                res.append(pos_cuadros[x])
+    return res
+
+
+if __name__ =="__main__":
+    cuadros = [0.5, 1.25, 3.5, 4.78, 6, 6.9, 10, 13, 18.30]
+    vision = 2.25
+    pared = 19.33
+
+    print(coloca_seguratas(cuadros, vision, pared))
